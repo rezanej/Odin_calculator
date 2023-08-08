dfae9e823b00b7edc2437f4be50eaa43246b2058
rezaLin <rezanej666@gmail.com>
2023-08-08 14:20:18 +0330

    Add operate Function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let operand0, operand1, operator;
+
 function add(num1,num2){
     return num1+num2;
 }
@@ -12,4 +14,23 @@ function multiply(num1,num2)
 function divide(num1,num2)
 {
     num1/num2;
+}
+function operate(num1,num2,operator)
+{
+    if (operator=="+")
+    {
+        return  add(num1,num2);
+    }
+    else if (operator=="-")
+    {
+        return subtract(num1,num2);
+    }
+    else if (operator=="/")
+    {
+        return divide(num1,num2);
+    }
+    else if (operator=="*")
+    {
+        return multiply(num1,num2);
+    }
 }
